9b98999d5ee39184dd8c3033d09153393c36f53c
TOBENTOch <info@tobento.ch>
2021-03-21 10:07:45 +0100

    More tests written

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -15,19 +15,63 @@ namespace Tobento\Service\HelperFunction\Test;
 
 use PHPUnit\Framework\TestCase;
 use Tobento\Service\HelperFunction\Functions;
+use RuntimeException;
 
 /**
  * Functions tests
  */
-class Functions extends TestCase
-{    
-    public function setUp(): void
+class FunctionsTest extends TestCase
+{
+    public function testRegisterFunctionFileFails()
     {
-        //
-    }
-
-    public function testFunctions()
-    {
-        //
+        // Create Functions
+        $functions = new Functions();
+        
+        $this->expectException(RuntimeException::class);
+        
+        // Register a function file.
+        $functions->register(__DIR__.'/Mock/missing_functions.php');
 	}
+
+    public function testMissingData()
+    {
+        // Create Functions
+        $functions = new Functions();
+        
+        // Register a function file.
+        $functions->register(__DIR__.'/Mock/functions.php');
+        
+        $this->expectException(RuntimeException::class);
+
+        locale();
+	}
+    
+    public function testCallingAFunction()
+    {
+        // Create Functions
+        $functions = new Functions();
+        
+        // Register a function file.
+        $functions->register(__DIR__.'/Mock/functions.php');
+        
+        // Set any data for later usage for your functions.
+        $functions->set('sitename', 'Your Sitename');
+
+        $this->assertSame('Your Sitename', sitename());
+	}
+    
+    public function testGetKeys()
+    {
+        // Create Functions
+        $functions = new Functions();
+        
+        // Register a function file.
+        $functions->register(__DIR__.'/Mock/functions.php');
+        
+        // Set any data for later usage for your functions.
+        $functions->set('sitename', 'Your Sitename');
+        $functions->set('foo', 'bar');
+
+        $this->assertSame(['sitename', 'foo'], $functions->getKeys());
+	}    
 }
